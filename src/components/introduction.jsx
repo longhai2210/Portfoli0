859d53bb84db5d22a9744c72b7513bbcdb467c22
longhai2210 <hai.phi@quant-edge.com>
2020-07-07 20:53:04 +0700

    more blue text

--- src/components/introduction.jsx
+++ src/components/introduction.jsx
@@ -13,9 +13,9 @@ export default class Introduction extends Component {
                   <div className="row">
                     <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
                       <div className="slider-text-inner js-fullheight">
-                        <div className="desc blue-text">
-                          <h1>Hi! <br />I'm Long Hai</h1>
-                          <p><a className="btn btn-primary btn-learn" href="https://docs.google.com/document/d/15U1C4Qwu3LrkhLzDay29wGqSChEfKwXGDOCGnoiA-vU/edit?usp=sharing" target="_blank" rel="noopener noreferrer">View CV<i className="icon-download4" /></a></p>
+                        <div className="desc">
+                          <h1 className="blue-text">Hi! <br />I'm Long Hai</h1>
+                          <p className="blue-text"><a className="btn btn-primary btn-learn" href="https://docs.google.com/document/d/15U1C4Qwu3LrkhLzDay29wGqSChEfKwXGDOCGnoiA-vU/edit?usp=sharing" target="_blank" rel="noopener noreferrer">View CV<i className="icon-download4" /></a></p>
                         </div>
                       </div>
                     </div>
@@ -28,9 +28,9 @@ export default class Introduction extends Component {
                   <div className="row">
                     <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
                       <div className="slider-text-inner">
-                        <div className="desc blue-text">
-                        <h1>I strive to <br/> improve myself ... </h1>
-                          <p><a className="btn btn-primary btn-learn" href="https://github.com/longhai2210" target="_blank" rel="noopener noreferrer">View Projects <i className="icon-briefcase3" /></a></p>
+                        <div className="desc">
+                        <h1 className="blue-text">I strive to <br/> improve myself ... </h1>
+                          <p><a className="btn btn-primary btn-learn blue-text" href="https://github.com/longhai2210" target="_blank" rel="noopener noreferrer">View Projects <i className="icon-briefcase3" /></a></p>
                         </div>
                       </div>
                     </div>
