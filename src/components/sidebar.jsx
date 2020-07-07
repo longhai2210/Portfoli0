f80dba08821925ec0d28ab06c7d0cf9a66fd8172
longhai2210 <hai.phi@quant-edge.com>
2020-07-07 21:20:54 +0700

    line break

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -30,7 +30,7 @@ export default class Sidebar extends Component {
                 <li><a href="https://github.com/longhai2210" target="_blank" rel="noopener noreferrer"><i className="icon-github"></i></a></li>
               </ul>
             </nav>
-            <br /><br />
+            <br />
             <div className="text-center">
               <span className="email">Made with ReactJs and Bootstrap</span>
             </div>
